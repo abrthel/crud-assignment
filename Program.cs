611bf66e3755c0dd231ea69dc67fbc0fc7cbc51b
Aaron Barthel <aaron.barthel@live.ca>
2020-08-16 12:28:01 -0600

    Allow user to list stored email addresses

--- Program.cs
+++ Program.cs
@@ -23,29 +23,33 @@ namespace CrudApp
                 "abby@msn.com", "jim@yahoo.com", "martin@live.ca", "fay@live.ca"
             };
 
-            Console.WriteLine("Type an action name or 'help' for more information. 'q' to quit.");
+            Console.WriteLine("Type an action name or 'help' for more information. 'q' to quit.\n\n");
+            string action = GetAction();
             do
             {
-                Console.WriteLine();
-                string action = Console.ReadLine();
                 try
                 {
-                    switch(action.Trim().ToLower())
+                    switch(action)
                     {
                         case "list":
                             List(emailAddresses);
+                            action = string.Empty;
                             break;
 
                         case "add":
-
+                            action = string.Empty;
                             break;
 
                         case "edit":
-
+                            action = string.Empty;
                             break;
 
                         case "remove":
+                            action = string.Empty;
+                            break;
 
+                        case "clear":
+                            action = string.Empty;
                             break;
 
                         case "quit":
@@ -54,18 +58,26 @@ namespace CrudApp
                             break;
 
                         case "help":
-                        default:
                             // I'm aware this creates a new array on each call and I should
                             // avoid this. However, I'd say its a toy app and this looks cleaner
                             // to me.
                             Console.WriteLine(string.Join(Environment.NewLine, new string[] {
+                                "+====== Help Menu ======+",
+                                "Select an action from the list below or type 'q' or 'quit' to quit the program.",
                                 "list   - Lists all of the email addresses",
                                 "add    - Adds a new email address",
                                 "edit   - Updates an email address",
                                 "remove - Removes an email address",
+                                "clear  - Removes all email addresses",
                                 "quit   - Quits the program",
                                 "help   - Displays this window",
                             }));
+                            action = string.Empty;
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid action...\n");
+                            action = "help";
                             break;
                     }
                 }
@@ -73,12 +85,36 @@ namespace CrudApp
                 {
                     Console.WriteLine("Error: {0}\n", ex.Message);
                 }
+
+                Console.Write("\n\n"); // Add space between outputs
+
+                // Get next action from user if next menu is not
+                // already set.
+                if(string.IsNullOrEmpty(action))
+                {
+                    action = GetAction();
+                }
             } while(!quit);
         }
 
+        static string GetAction()
+        {
+            Console.Write("Select an action: ");
+            return Console.ReadLine().ToLower().Trim();
+        }
+        /// <summary>
+        /// Displays all of the email addresses in the console for the user to see.
+        /// </summary>
+        /// <param name="emailAddresses">List of email addresses to display.</param>
         static void List(List<string> emailAddresses)
         {
-
+            Console.WriteLine("Email Addresses: ");
+            int count = 1;
+            foreach(string emailAddress in emailAddresses)
+            {
+                Console.WriteLine($"{count}. {emailAddress}");
+                count++;
+            }
         }
 
 
